fafaf90a3de89473f52a01d62cf0a891c062c94d
theperrinpages <60055497+theperrinpages@users.noreply.github.com>
2020-03-24 02:21:06 +1100

    the first upload

---
+++ line counter/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace line_counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                StreamReader reader = new StreamReader(@"C:\Users\root\Desktop\rockyou.txt");
+                int linecount = 0;
+                while (reader.ReadLine() != null)
+                {
+                    linecount++;
+                }
+
+                Console.WriteLine("Line count: {0}", linecount.ToString());
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+            
+
+        }
+    }
+}
